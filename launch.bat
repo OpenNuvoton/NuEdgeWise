0ec83663db1dfc45112733b8bf544206a58f776e
MaxCYCHEN <CYCHEN38@nuvoton.com>
2023-11-02 13:14:14 +0800

    update launch.bat to miniforge

--- launch.bat
+++ launch.bat
@@ -1,7 +1,10 @@
-@ ECHO OFF
-set MODEL_SRC_DIR=C:\ProgramData\Anaconda3
+@ ECHO On
+set MODEL_SRC_DIR=C:\ProgramData\miniforge3
 call %MODEL_SRC_DIR%\Scripts\activate.bat
-call cd c:\
-::call cd %~dp0
-call conda activate NuEdgeWise_env
-call jupyter lab
+::call cd c:\
+call cd %~dp0
+call conda create --name NuEdgeWise_env_test  python=3.8.13
+call conda activate NuEdgeWise_env_test
+call pip install -r requirements.txt
+::call jupyter lab
+pause
